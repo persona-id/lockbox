70ad5c415c62292d0acafb8993b15e85645dea53
Andrew Kane <andrew@chartkick.com>
2019-06-07 17:25:46 -0700

    Rails 6 fix

--- lib/lockbox/utils.rb
+++ lib/lockbox/utils.rb
@@ -16,5 +16,37 @@ class Lockbox
     def self.encrypted_options(record, name)
       record.class.respond_to?(:encrypted_attachments) && record.class.encrypted_attachments[name.to_sym]
     end
+
+    def self.encrypted?(record, name)
+      encrypted_options(record, name).present?
+    end
+
+    def self.encrypt_attachable(record, name, attachable)
+      options = encrypted_options(record, name)
+      box = build_box(record, options)
+
+      case attachable
+      when ActiveStorage::Blob
+        raise NotImplementedError, "Not supported"
+      when ActionDispatch::Http::UploadedFile, Rack::Test::UploadedFile
+        attachable = {
+          io: StringIO.new(box.encrypt(attachable.read)),
+          filename: attachable.original_filename,
+          content_type: attachable.content_type
+        }
+      when Hash
+        attachable = {
+          io: StringIO.new(box.encrypt(attachable[:io].read)),
+          filename: attachable[:filename],
+          content_type: attachable[:content_type]
+        }
+      when String
+        raise NotImplementedError, "Not supported"
+      else
+        nil
+      end
+
+      attachable
+    end
   end
 end
